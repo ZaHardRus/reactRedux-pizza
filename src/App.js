58d8d6a72607101b2427f9e51ef8fbf5249c5abd
Denis Ackerman <zahardrus@gmail.com>
2021-10-19 21:43:20 +0300

    fix route

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import './scss/app.scss'
 import {Header} from "./component/Header/Header";
 import {MainPage} from "./pages/MainPage";
 import {Cart} from "./pages/Cart";
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 
 function App() {
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
-                <Route exact path={'/'}>
-                    <MainPage/>
-                </Route>
-                <Route path={'/cart'}>
-                    <Cart/>
-                </Route>
+                <Switch>
+                    <Route exact path={'/'}>
+                        <MainPage/>
+                    </Route>
+                    <Route exact path={'/cart'}>
+                        <Cart/>
+                    </Route>
+                </Switch>
             </div>
         </div>
     );
