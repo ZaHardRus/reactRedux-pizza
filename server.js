e9b0f31820f74e533894978580ca989d8e4877f4
Denis Ackerman <zahardrus@gmail.com>
2021-10-19 23:07:30 +0300

    fix route

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const middlewares = jsonServer.defaults({
 
 const PORT = process.env.PORT || 3000;
 server.get("/cart", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "public"));
+    res.sendFile(path.resolve(__dirname, "build"));
 });
 server.use(middlewares);
 server.use(router);
