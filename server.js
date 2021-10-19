bb2026f0ab3e1c5f5027cd6a64e2950bb80a66e0
Denis Ackerman <zahardrus@gmail.com>
2021-10-19 21:04:06 +0300

    server

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const path = require('path')
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001;
+const router = jsonServer.router('./public/db.json');
+const middlewares = jsonServer.defaults({
+    static: './build',
+});
 
+const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
-
-server.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./build", "index.html"));
-})
-server.listen(port,()=>console.log('server is running' + port))
+server.listen(PORT, () => {
+    console.log('Server is running');
+});
